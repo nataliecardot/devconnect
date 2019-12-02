6bcc25e0b5f91cff94d48f8bcaf2ecc8f6911ceb
Natalie-Cardot <nataliecardot@gmail.com>
2019-12-01 22:15:38 -0800

    Set validation with express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route POST api/users (JSDoc comment style)
 // @desc Register user
 // @access Public (don't need to send authentication token for it to work)
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('User route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    console.log(req.body);
+    res.send('User route');
+  }
+);
 
 module.exports = router;
