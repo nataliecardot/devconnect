1e6bc40bc5b573ea4349614629d2253d34954da2
Natalie-Cardot <nataliecardot@gmail.com>
2019-12-05 22:51:13 -0800

    Create profile model

---
+++ models/Profile.js
@@ -0,0 +1,118 @@
+const mongoose = require('mongoose');
+
+// Will bring this into profile routes and query the database (get profiles, create them, update, delete, etc.)
+const ProfileSchema = new mongoose.schema({
+  // Create reference to user model, because every profile model should be associated with user
+  user: {
+    // id is not in the User model file, but refers to user.id in the payload when a new user is created
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  // e.g., developer, senior developer, student, instructor (there will be a dropdown with options)
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    // An array of strings
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('Profile', ProfileSchema);
