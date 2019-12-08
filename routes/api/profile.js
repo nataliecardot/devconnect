8de4772ab7dd526ed33e30e0aca7f1d7e9077125
Natalie-Cardot <nataliecardot@gmail.com>
2019-12-08 15:31:58 -0800

    Add route to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,29 @@ const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
+// @route GET api/profile/me
+// @desc Get logged in user's profile
+// @access Private (getting profile by the user ID in the token, so a token must be provided)
+router.get('/me', auth, async (req, res) => {
+  try {
+    // User pertains to profile model user field, which is object ID of user
+    // populate method, which lets you reference documents in other collections, adds name and avatar to query from User model
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('User', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    // The message property is a human-readable description of the error
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 // @route POST api/profile/me
 // @desc Create or update a user profile
 // @access Private (getting profile by the user ID in the token, so a token must be provided)
@@ -98,26 +121,16 @@ router.post(
   }
 );
 
-// @route GET api/profile/me
-// @desc Get logged in user's profile
-// @access Private (getting profile by the user ID in the token, so a token must be provided)
-router.get('/me', auth, async (req, res) => {
+// @route GET api/profile
+// @desc Get all profiles
+// @access Public
+router.get('/', async (req, res) => {
   try {
-    // User pertains to profile model user field, which is object ID of user
-    // populate method, which lets you reference documents in other collections, adds name and avatar to query from User model
-    const profile = await Profile.findOne({
-      user: req.user.id
-    }).populate('User', ['name', 'avatar']);
-
-    if (!profile) {
-      return res.status(400).json({ msg: 'There is no profile for this user' });
-    }
-
-    res.json(profile);
+    const profiles = await Profile.find().populate('User', ['name', 'avatar']);
+    res.json(profiles);
   } catch (err) {
-    // The message property is a human-readable description of the error
     console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send('Server Error');
   }
 });
 
