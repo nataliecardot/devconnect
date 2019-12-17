1abc2470a60482dc336d3a0e6318262aebaadd6c
Natalie-Cardot <nataliecardot@gmail.com>
2019-12-17 14:18:59 -0800

    Add route for deleting a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -215,4 +215,39 @@ router.post(
   }
 );
 
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Delete comment
+// @access Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    // Check whether comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Check that user deleting the comment is user that made the comment
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {}
+});
+
 module.exports = router;
