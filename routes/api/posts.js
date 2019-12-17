d9dec9daae7a7bcfb2a96d449871ad3753ecf36b
Natalie-Cardot <nataliecardot@gmail.com>
2019-12-16 20:44:42 -0800

    Add route for adding a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -171,4 +171,48 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+// @route POST api/posts/comment/:id
+// @desc Comment on a post
+// @access Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Doing this so can get name, avatar (name and avatar are fetched from database using id from token [i.e., from the matching document in the users collection]), and user itself
+      // Logged in, so have token, which provides user id and puts in req.user.id
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
 module.exports = router;
