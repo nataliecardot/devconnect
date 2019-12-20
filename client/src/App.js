8014083b8449719b7ca29fe87bf3ea7b83496a77
Natalie-Cardot <nataliecardot@gmail.com>
2019-12-20 13:22:37 -0800

    Implement React Router, add auth components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+// Selectively imports BrowserRouter, Route and Link from react-router-dom. The as Router statement makes BrowserRouter available under the name Router (instead of BrowserRouter), while the names of Route and Link are not changed
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Landing from './components/layouts/Landing';
 import Navbar from './components/layouts/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => (
-  <>
+  <Router>
     <Navbar />
-    <Landing />
-  </>
+    {/* exact prop needed to prevent all routes starting with "/" to render */}
+    <Route exact path="/" component={Landing} />
+    {/* Every page within theme except landing page has a class of container to center everything */}
+    <section className="container">
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </section>
+  </Router>
 );
 
 export default App;
