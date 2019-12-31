cc9b00ccd8a40ebd8b0de5d32c4a8dee80620f75
Natalie-Cardot <nataliecardot@gmail.com>
2019-12-30 23:08:18 -0800

    CreateProfile component form state setup

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -18,6 +18,8 @@ const CreateProfile = props => {
     instagram: ''
   });
 
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
   const {
     company,
     website,
@@ -33,7 +35,9 @@ const CreateProfile = props => {
     instagram
   } = formData;
 
-  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+  // Square brackets in an object are used when you need to use a variable/attribute as a key (property) in a key-value pair. ES6 computed property name syntax https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#Computed_property_names
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
   return (
     <>
@@ -45,7 +49,11 @@ const CreateProfile = props => {
       <small>* = required field</small>
       <form className="form">
         <div className="form-group">
-          <select name="status">
+          <select
+            name="status"
+            value={status}
+            onChange={() => onChange(e => onChange(e))}
+          >
             <option value="0">* Select professional status</option>
             <option value="Developer">Developer</option>
             <option value="Junior developer">Junior developer</option>
@@ -61,25 +69,49 @@ const CreateProfile = props => {
           </small>
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Company" name="company" />
+          <input
+            type="text"
+            placeholder="Company"
+            name="company"
+            value={company}
+            onChange={e => onChange(e)}
+          />
           <small className="form-text">
             Could be your own company or one you work for
           </small>
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Website" name="website" />
+          <input
+            type="text"
+            placeholder="Website"
+            name="website"
+            value={website}
+            onChange={e => onChange(e)}
+          />
           <small className="form-text">
             Could be your own or a company website
           </small>
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Location" name="location" />
+          <input
+            type="text"
+            placeholder="Location"
+            name="location"
+            value={location}
+            onChange={e => onChange(e)}
+          />
           <small className="form-text">
             City & state suggested (e.g., Boston, MA)
           </small>
         </div>
         <div className="form-group">
-          <input type="text" placeholder="* Skills" name="skills" />
+          <input
+            type="text"
+            placeholder="* Skills"
+            name="skills"
+            value={skills}
+            onChange={e => onChange(e)}
+          />
           <small className="form-text">
             Please use comma-separated values (e.g., HTML, CSS, JavaScript, PHP)
           </small>
@@ -89,6 +121,8 @@ const CreateProfile = props => {
             type="text"
             placeholder="GitHub username"
             name="githubusername"
+            value={githubusername}
+            onChange={e => onChange(e)}
           />
           <small className="form-text">
             If you want your latest repos and a GitHub link, include your
@@ -96,41 +130,88 @@ const CreateProfile = props => {
           </small>
         </div>
         <div className="form-group">
-          <textarea placeholder="A short bio of yourself" name="bio"></textarea>
+          <textarea
+            placeholder="A short bio of yourself"
+            name="bio"
+            value={bio}
+            onChange={e => onChange(e)}
+          ></textarea>
           <small className="form-text">Tell us a little about yourself</small>
         </div>
 
         <div className="my-2">
-          <button type="button" className="btn btn-light">
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type="button"
+            className="btn btn-light"
+          >
             Add Social Network Links
           </button>
-          <span>Optional</span>
+          <br />
+          {!displaySocialInputs && (
+            <small className="form-text">Optional</small>
+          )}
         </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-twitter fa-2x"></i>
-          <input type="text" placeholder="Twitter URL" name="twitter" />
-        </div>
+        {displaySocialInputs && (
+          <>
+            <div className="form-group social-input">
+              <i className="fab fa-twitter fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Twitter URL"
+                name="twitter"
+                value={twitter}
+                onChange={e => onChange(e)}
+              />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-facebook fa-2x"></i>
-          <input type="text" placeholder="Facebook URL" name="facebook" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-facebook fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Facebook URL"
+                name="facebook"
+                value={facebook}
+                onChange={e => onChange(e)}
+              />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-youtube fa-2x"></i>
-          <input type="text" placeholder="YouTube URL" name="youtube" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-youtube fa-2x"></i>
+              <input
+                type="text"
+                placeholder="YouTube URL"
+                name="youtube"
+                value={youtube}
+                onChange={e => onChange(e)}
+              />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-linkedin fa-2x"></i>
-          <input type="text" placeholder="LinkedIn URL" name="linkedin" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-linkedin fa-2x"></i>
+              <input
+                type="text"
+                placeholder="LinkedIn URL"
+                name="linkedin"
+                value={linkedin}
+                onChange={e => onChange(e)}
+              />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-instagram fa-2x"></i>
-          <input type="text" placeholder="Instagram URL" name="instagram" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-instagram fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Instagram URL"
+                name="instagram"
+                value={instagram}
+                onChange={e => onChange(e)}
+              />
+            </div>
+          </>
+        )}
+
         <input type="submit" className="btn btn-primary my-1" />
         <a className="btn btn-light my-1" href="dashboard.html">
           Go Back
