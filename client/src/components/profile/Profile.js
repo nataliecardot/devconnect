fd9e66f529ea02935a26b2d4af91a6d58937c9c5
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-05 13:20:09 -0800

    Get started on profile

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../actions/profile';
+
+// Getting id from URL. You can get it from props.match.params.[whatever the param is, in this case id]. Destructuring it out of props (could destructure further)
+const Profile = ({
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+  match
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById]);
+
+  return (
+    <>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to="/profiles" className="btn btn-light">
+            Back to Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </>
+      )}
+    </>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  // Need this because if user is logged in and profile is their own, will display an edit profile button
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile);
