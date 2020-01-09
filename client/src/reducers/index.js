81ae106d01b7e0f3cd11bb172a2cf9c31aaa9c93
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-09 14:59:09 -0800

    Add post reducer, action, and initial component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
+import post from './post';
 
 // Root reducer
 export default combineReducers({
   alert,
   auth,
-  profile
+  profile,
+  post
 });
