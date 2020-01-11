97f9e9ab235acbddb8df8b869c52f7ade6d53540
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-11 14:10:01 -0800

    Enable display of single post (post discussion)

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
+import { getPost } from '../../actions/post';
+
+// React Router supplies any dynamic pieces of the URL to the component via an object called match.params as own props of the related component (the route for this component has path="/post:id")
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <Link to="/posts" className="btn">
+        Back to Post
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -10,7 +10,8 @@ const PostItem = ({
   removeLike,
   deletePost,
   auth,
-  post: { _id, text, name, avatar, user, likes, comments, date }
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  showActions
 }) => (
   <div className="post bg-white p-1 my-1">
     <div>
@@ -24,42 +25,51 @@ const PostItem = ({
       <p className="post-date">
         Posted on <Moment format="MM-DD-YYYY">{date}</Moment>
       </p>
-      <button
-        onClick={() => addLike(_id)}
-        type="button"
-        className="btn btn-light"
-      >
-        <i
-          className="fas fa-thumbs-up"
-          style={{ paddingRight: likes.length > 0 ? '0.25rem' : null }}
-        />
-        {likes.length > 0 && <span>{likes.length}</span>}
-      </button>
-      <button
-        onClick={() => removeLike(_id)}
-        type="button"
-        className="btn btn-light"
-      >
-        <i className="fas fa-thumbs-down" />
-      </button>
-      <Link to={`/post/${_id}`} className="btn btn-primary">
-        Discussion{' '}
-        {comments.length > 0 && (
-          <span className="comment-count">{comments.length}</span>
-        )}
-      </Link>
-      {!auth.loading && user === auth.user._id && (
-        <button
-          onClick={() => deletePost(_id)}
-          type="button"
-          className="btn btn-danger"
-        >
-          <i className="fas fa-times" />
-        </button>
+
+      {showActions && (
+        <>
+          <button
+            onClick={() => addLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i
+              className="fas fa-thumbs-up"
+              style={{ paddingRight: likes.length > 0 ? '0.25rem' : null }}
+            />
+            {likes.length > 0 && <span>{likes.length}</span>}
+          </button>
+          <button
+            onClick={() => removeLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i className="fas fa-thumbs-down" />
+          </button>
+          <Link to={`/posts/${_id}`} className="btn btn-primary">
+            Discussion{' '}
+            {comments.length > 0 && (
+              <span className="comment-count">{comments.length}</span>
+            )}
+          </Link>
+          {!auth.loading && user === auth.user._id && (
+            <button
+              onClick={() => deletePost(_id)}
+              type="button"
+              className="btn btn-danger"
+            >
+              <i className="fas fa-times" />
+            </button>
+          )}
+        </>
       )}
     </div>
   </div>
 );
+
+PostItem.defaultProps = {
+  showActions: true
+};
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
