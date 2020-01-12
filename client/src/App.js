b5f43486044b4732a99af58b1f0634377e2c0fb4
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-12 13:11:57 -0800

    Add NotFound page

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,10 @@ import React, { useEffect } from 'react';
 // Selectively imports BrowserRouter, Route and Link from react-router-dom. The as Router statement makes BrowserRouter available under the name Router (instead of BrowserRouter), while the names of Route and Link are not changed
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-forms/CreateProfile';
-import EditProfile from './components/profile-forms/EditProfile';
-import AddExperience from './components/profile-forms/AddExperience';
-import AddEducation from './components/profile-forms/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
-import PrivateRoute from './components/routing/PrivateRoute';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -37,36 +27,12 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Navbar />
-        {/* exact prop needed to prevent all routes starting with "/" to render */}
-        <Route exact path="/" component={Landing} />
+        <Switch>
+          {/* exact prop needed to prevent all routes starting with "/" to render */}
+          <Route exact path="/" component={Landing} />
+          <Route component={Routes} />
+        </Switch>
         {/* Every page within theme except landing page has a class of container to center everything */}
-        <div className="container">
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/profiles" component={Profiles} />
-            <Route exact path="/profile/:id" component={Profile} />
-            <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            <PrivateRoute
-              exact
-              path="/create-profile"
-              component={CreateProfile}
-            />
-            <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-            <PrivateRoute
-              exact
-              path="/add-experience"
-              component={AddExperience}
-            />
-            <PrivateRoute
-              exact
-              path="/add-education"
-              component={AddEducation}
-            />
-            <PrivateRoute exact path="/posts" component={Posts} />
-            <PrivateRoute exact path="/posts/:id" component={Post} />
-          </Switch>
-        </div>
       </Router>
     </Provider>
   );
---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return (
+    <div className="narrow-container">
+      <h1 className="x-large text-primary my-3 text-center">
+        <i className="fas fa-exclamation-triangle" /> Page Not Found
+      </h1>
+
+      <p className="large text-center">Sorry, this page does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
