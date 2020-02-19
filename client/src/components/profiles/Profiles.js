4dbb20cfa6ef778a4ffc07774444c559f18e0b10
Natalie-Cardot <nataliecardot@gmail.com>
2020-02-18 20:54:18 -0800

    Add logic to display current page's profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
@@ -6,9 +6,25 @@ import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profile';
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [profilesPerPage, setProfilesPerPage] = useState(10);
+
   useEffect(() => {
     getProfiles();
   }, [getProfiles]);
+
+  // First/last profile index not on the page but to use in the slice method
+  // For first page, would be 1 x 10 = 10; for second page, 2 x 10 = 20
+  const indexOfLastProfile = currentPage * profilesPerPage;
+  // For first page, would be 10 - 10 = 0; for second page, 20 - 10 = 10
+  const indexOfFirstProfile = indexOfLastProfile - profilesPerPage;
+  // Slice method returns new array, selecting elements starting at the given start argument, up until (but excluding) the given end argument
+  const currentProfiles = profiles.slice(
+    indexOfFirstProfile,
+    indexOfLastProfile
+  );
+
+  console.log(currentProfiles[0]);
 
   return (
     <>
@@ -18,8 +34,8 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
         developers
       </p>
       <div className="profiles">
-        {profiles.length > 0 ? (
-          profiles.map(profile => (
+        {currentProfiles.length > 0 ? (
+          currentProfiles.map(profile => (
             <ProfileItem key={profile._id} profile={profile} />
           ))
         ) : (
